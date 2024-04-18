e1621a60ec237eae5b347474ebe8ab3251cb782b
Jace Roldan <jaceroldan16@gmail.com>
2024-04-18 13:57:09 +0800

    Correct index-test3.js

--- index-test3.js
+++ index-test3.js
@@ -157,7 +157,8 @@ class MessageWidget {
             // accessed through data-param1
             // const scriptTag = document.querySelector('script[src*="https://cdn.jsdelivr.net/gh/jaceroldan-ai"]');
             // const scriptTag = document.querySelector('script[src*="https://cdn.jsdelivr.net/gh/jaceroldan-ai"]');
-            const scriptTag = document.querySelectorAll('script');
+            const scriptTags = document.querySelectorAll('script');
+            scriptTag = scriptTags.filter(st => st.attributes.includes('data-param'));
             console.log('scriptTag', scriptTag);
             const pk = String(482);
             const baseUrl = 'http://localhost:8000/api-sileo/v1/hqzen/engagement-form-document-resource/get/';
